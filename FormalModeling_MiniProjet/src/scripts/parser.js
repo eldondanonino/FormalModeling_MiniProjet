b2a110cdf7f5f5945277dcc4ac1ae256f0717e0d
Daniil ROSSO <daniil.rosso@efrei.net>
2022-01-18 21:58:52 +0100

    Progress commit

--- FormalModeling_MiniProjet/src/scripts/parser.js
+++ FormalModeling_MiniProjet/src/scripts/parser.js
@@ -24,16 +24,39 @@ const fileName = "test1";
 const finalPath = "".concat(filePath, fileName, ".txt");
 
 // Display elements
-export function output(textByLine) {
-  console.log(textByLine);
+export function output(data) {
+  // console.log(data);
+  document.getElementById("app").innerHTML =
+    "<h1> " +
+    data[0] +
+    "</h1><br/>" +
+    "<h1> " +
+    data[1] +
+    "</h1><br/>" +
+    "<h1> " +
+    data[2] +
+    "</h1><br/>" +
+    "<h1> " +
+    data[3] +
+    "</h1><br/>" +
+    "<h1> " +
+    data[4] +
+    "</h1><br/>";
+}
 
-  textByLine.forEach(function callback(value, index, object) {
-    if (value.match(/^KRP|^S:|^L:|^T:|^I:|^CTL/)) {
-      object.splice(index, 1); //removes indicators
-    }
+export function process(input) {
+  let states = input[0].replace(/{|}/g, "").split(",");
+  console.log("States: " + states);
+
+  let tuples = input[1].replace(/\)|{|}/g, "").split("(");
+  tuples.filter(function(e){return e});
+  tuples.forEach(function pairs(value, index, object) {
+    object[index] = object[index].split(',');
+    object[index] = object[index].filter(function(e){return e});
   });
-  console.log(textByLine);
-  document.getElementById("app").innerHTML = "<h1> " + textByLine + "</h1>";
+  tuples.shift();
+  console.log("tuples: ");
+  console.log(tuples);
 }
 
 //parse();
@@ -43,6 +66,16 @@ export function parse() {
   let text = fs.readFileSync(finalPath, "utf-8");
   // Put content in table
   let textByLine = text.split("\r\n");
-
+  textByLine.forEach(function callback(value, index, object) {
+    if (value.match(/^KRP|^S:|^L:|^T:|^I:|^CTL/)) {
+      object.splice(index, 1); //removes indicators
+    }
+  });
+  const elements = textByLine[0];
+  const values_tuples = textByLine[1];
+  const transitions = textByLine[2];
+  const initial_state = textByLine[3];
+  const ctl = textByLine[4];
+  process(textByLine);
   return textByLine;
 }

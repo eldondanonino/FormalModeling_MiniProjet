9fb4f0c24be783224fdf7de9f7ec64ae35943423
Daniil ROSSO <daniil.rosso@efrei.net>
2022-01-18 22:06:49 +0100

    Processed data

--- FormalModeling_MiniProjet/src/scripts/parser.js
+++ FormalModeling_MiniProjet/src/scripts/parser.js
@@ -49,14 +49,35 @@ export function process(input) {
   console.log("States: " + states);
 
   let tuples = input[1].replace(/\)|{|}/g, "").split("(");
-  tuples.filter(function(e){return e});
+  tuples.filter(function (e) {
+    return e;
+  });
   tuples.forEach(function pairs(value, index, object) {
-    object[index] = object[index].split(',');
-    object[index] = object[index].filter(function(e){return e});
+    object[index] = object[index].split(",");
+    object[index] = object[index].filter(function (e) {
+      return e;
+    });
   });
   tuples.shift();
   console.log("tuples: ");
   console.log(tuples);
+
+  let transitions = input[2].replace(/\),|\)|{|}/g, "").split("(");
+  transitions.forEach(function pairs(value, index, object) {
+    object[index] = object[index].split(",");
+  });
+  transitions.shift();
+  console.log("transitions: ");
+  console.log(transitions);
+
+  let initial_state = input[3].replace(/{|}/g, "");
+
+  console.log("initial state: ");
+  console.log(initial_state);
+
+  let ctl = input[4];
+  console.log("ctl: ");
+  console.log(ctl);
 }
 
 //parse();
